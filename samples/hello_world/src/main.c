ef39c89db9ecc7033ace173b289cb6ac2f8aa867
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2024-03-22 16:12:47 -0400

    Testing the new Kconfig app version variables
    ```bash
    ❯ west build -p always -b qemu_cortex_m3 zephyr/samples/hello_world/ -t run
    ...
    Kconfig available APP_VERSION identifiers:
    VERSION_MAJOR=1 VERSION_MINOR=0 PATCHLEVEL=0 VERSION_TWEAK=0 APPVERSION=1.0.0 APP_VERSION_EXTENDED_STRING=1.0.0+0 APP_VERSION_TWEAK_STRING=1.0.0+
    ```

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,10 +5,16 @@
  */
 
 #include <stdio.h>
+#include <zephyr/toolchain.h>
+#include <zephyr/kernel.h>
+#include <app_version.h>
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	printk("Kconfig available APP_VERSION identifiers:\n");
+	printk("%s\n", CONFIG_APP_VERSION);
 
 	return 0;
 }
