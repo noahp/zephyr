893ef3407604a3abbe3ae55f74c2b29202edde5e
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2025-03-18 10:22:17 -0400

    RT685 reboot reason test

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -20,6 +20,44 @@
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
+// Add a simple shell command that does nothing
+
+#include <zephyr/shell/shell.h>
+
+#include "fsl_device_registers.h"
+
+static int cmd_rebootinfo(const struct shell *shell, size_t argc, char **argv)
+{
+	const uint32_t reset_stat_reg = RSTCTL0->SYSRSTSTAT;
+	shell_print(shell, "Reset status register: 0x%08x", reset_stat_reg);
+	if (reset_stat_reg & RSTCTL0_SYSRSTSTAT_VDD_POR_MASK) {
+		shell_print(shell, "VDD_POR");
+	}
+	if (reset_stat_reg & RSTCTL0_SYSRSTSTAT_PAD_RESET_MASK) {
+		shell_print(shell, "PAD_RESET");
+	}
+	if (reset_stat_reg & RSTCTL0_SYSRSTSTAT_ARM_APD_RESET_MASK) {
+		shell_print(shell, "ARM_APD_RESET");
+	}
+	if (reset_stat_reg & RSTCTL0_SYSRSTSTAT_WDT0_RESET_MASK) {
+		shell_print(shell, "WDT0_RESET");
+	}
+	if (reset_stat_reg & RSTCTL0_SYSRSTSTAT_WDT1_RESET_MASK) {
+		shell_print(shell, "WDT1_RESET");
+	}
+	return 0;
+}
+SHELL_CMD_REGISTER(rebootinfo, NULL, "Print reboot info", cmd_rebootinfo);
+
+// simple command to clear reboot info
+static int cmd_clearrebootinfo(const struct shell *shell, size_t argc, char **argv)
+{
+	RSTCTL0->SYSRSTSTAT = 0xFFFFFFFF;
+	shell_print(shell, "Reset status register cleared");
+	return 0;
+}
+SHELL_CMD_REGISTER(clearrebootinfo, NULL, "Clear reboot info", cmd_clearrebootinfo);
+
 int main(void)
 {
 	int ret;
@@ -41,7 +79,7 @@ int main(void)
 		}
 
 		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		// printf("LED state: %s\n", led_state ? "ON" : "OFF");
 		k_msleep(SLEEP_TIME_MS);
 	}
 	return 0;
